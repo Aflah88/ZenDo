00faf9dd0b16964c7dc0af96a79a042fc9987436
Hendi Setiawan <hendisetiawan151004@gmail.com>
2026-01-17 21:59:18 +0700

    fix: perbaiki error import

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Navbar } from "@/app/components";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ZenDo - Productivity App",
-  description: "Manajemen tugas mahasiswa dengan indikator stres.",
+  title: "ZenDo",
+  description: "Productivity App",
 };
 
 export default function RootLayout({
@@ -16,12 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" className="scroll-smooth">
-      <body className={`${inter.className} antialiased text-slate-800 bg-gray-50 selection:bg-indigo-100 selection:text-indigo-900`}>
+    <html lang="id">
+      <body className={`${inter.className} antialiased bg-gray-50 text-slate-900`}>
         <Navbar />
+
         <main className="min-h-screen">
           {children}
         </main>
+
       </body>
     </html>
   );
