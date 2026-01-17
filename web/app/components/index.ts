9ad301c165a501628940684b4354e03bcc859d48
Hendi Setiawan <hendisetiawan151004@gmail.com>
2026-01-17 20:44:05 +0700

    chore: export komponen footer dari index barrel agar bisa diakses global

--- web/app/components/index.ts
+++ web/app/components/index.ts
@@ -2,3 +2,4 @@ export { TaskForm } from './TaskForm';
 export { StressIndicator } from './StressIndicator';
 export { Statistics } from './Statistics';
 export { TaskList } from './TaskList';
+export { Footer } from './Footer';
