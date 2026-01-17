b04e23ff55851b129ee5d16a3aaa06f6ae57619f
Hendi Setiawan <hendisetiawan151004@gmail.com>
2026-01-17 21:06:15 +0700

    chore: export navbar component agar bisa digunakan di layout global

--- web/app/components/index.ts
+++ web/app/components/index.ts
@@ -3,3 +3,4 @@ export { StressIndicator } from './StressIndicator';
 export { Statistics } from './Statistics';
 export { TaskList } from './TaskList';
 export { Footer } from './Footer';
+export { Navbar } from './Navbar';
