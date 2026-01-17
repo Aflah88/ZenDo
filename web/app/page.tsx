2333b54dbec37abbab2214f9623bae7b3b455339
Hendi Setiawan <hendisetiawan151004@gmail.com>
2026-01-17 20:05:13 +0700

    fix: perbaiki path import dan update syntax gradien tailwind

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Statistics, StressIndicator, TaskForm, TaskList } from "@/components";
-import { useTasks } from "@/hooks/useTasks";
+import { Statistics, StressIndicator, TaskForm, TaskList } from "./components";
+import { useTasks } from "../hooks/useTasks";
 import { useState } from "react";
 
 export default function Home() {
@@ -32,7 +32,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-6 md:p-8">
+    <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 p-6 md:p-8">
       <div className="max-w-4xl mx-auto">
         {/* Header */}
         <div className="text-center mb-8">
